fdce1d1cd0ce6332a522a7367a5f6c2d1ee3c04a
Georg <stowaway000@yandex.ru>
2019-04-21 17:33:00 +0300

    Исправлена передача в конструктор классов имени предмета

--- models.py
+++ models.py
@@ -5,9 +5,10 @@ import pyglet
 from pyglet.image import load, ImageGrid, Animation
 from pyglet.window import key, mouse
 
-class item():
-    def __init__(self, sprite_name, weight, cost):
-        self.sprite_name = sprite_name 
+
+class item(cocos.sprite.Sprite):
+    def __init__(self, name, weight, cost):
+        self.sprite_name = name + ".png"
         self.weight = weight
         self.cost = cost
 
@@ -15,24 +16,25 @@ class item():
 class armor(item):
     # 0 <= mac_ac <= 100
     # 0 <= def_firearm <= 0.99
-    def __init__(self, max_ac, def_firearm, sprite_name, weight, cost):
-        super().__init__(sprite_name, weight, cost)
-        self.max_ac = max_ac # max_ac - максимальная прочность брони
-        self.def_firearm = def_firearm # def_firearm - защита от огнестрельного оружия
-        self.ac = max_ac  # ac - текущая прочность брони
+    def __init__(self, armor_name, max_ac, def_firearm, sprite_name, weight, cost):
+        super().__init__(armor_name, weight, cost)
+        self.max_ac = max_ac  # max_ac - максимальная прочность брони
+        self.def_firearm = def_firearm  # def_firearm - защита от огнестрельного оружия
+        self.ac = max_ac # ac - текущая прочность брони
 
 
 class armor_handler():
-    def __init__(ac, armor_name):
+    def __init__(armor_name):
         self.armor_name = armor_name
         self.ac = items[armor_name].ac
-    
+        self.def_firearm = items[armor_name].def_firearm
+
     def get_damage(self, dmg=1, k=1):
         # dmg - кол-во урона
         # k - коэффициент пробития
         # mainHero.hp -= dmg * k * (1 - self.def_firearm)
         mainHero.hp -= dmg
-    
+
     def statusAC(self, dmg=1, k=1):
         # dmg - кол-во урона
         # k - коэффициент пробития
@@ -40,52 +42,55 @@ class armor_handler():
         self.ac -= 1
         if self.ac <= 0:
             self.def_firearm = 0
-            
-            
+
+
 class weapon(item):
     # stats = [damage, breachness, max_cartridge]
     # anim_name - sprite анимации
-    def __init__(self, stats, sprite_name, anim_name, weight, cost, height_anim, width_anim, count_anim):
-        super().__init__(sprite_name, weight, cost)
-        self.anim_name = anim_name
-        self.damage = stats[0] # damage - урон
-        self.breachness = stats[1] # breachness - пробивная способность
-        self.max_cartridge = stats[2] # max_cartridge - размер обоймы
-        
+    def __init__(self, weapon_name, stats, weight, cost, height_anim, width_anim, count_anim):
+        super().__init__(weapon_name, weight, cost)
+        self.anim_name = weapon_name + "_anim.png"
+        self.damage = stats[0]  # damage - урон
+        self.breachness = stats[1]  # breachness - пробивная способность
+        self.max_cartridge = stats[2]  # max_cartridge - размер обоймы
+
         self.count_anim = count_anim
         self.width_anim = width_anim
         self.height_anim = height_anim
-        
+
         self.weapon_sprite = Sprite(self.sprite_name, scale=8)
         self.weapon_sprite.position = 400, 400
+        test_shoot.add(self.weapon_sprite)
         
         shoot_img = load(self.anim_name)
         shoot_grid = ImageGrid(shoot_img, 1,
-                                   self.count_anim,
-                                   item_height=self.height_anim,
-                                   item_width=self.width_anim)
-        self.weapon_anim = Animation.from_image_sequence(shoot_grid[:], 0.05, loop=False)        
+                               self.count_anim,
+                               item_height=self.height_anim,
+                               item_width=self.width_anim)
+        self.weapon_anim = Animation.from_image_sequence(shoot_grid[:], 0.05, loop=False)
 
 
 class weapon_handler(cocos.layer.Layer):
     is_event_handler = True
+
     def __init__(self, weapon_name):
         super().__init__()
         self.weapon_name = weapon_name
         self.cartridge = 0
         self.anim_name = items[weapon_name].anim_name
         self.sprite_name = items[weapon_name].sprite_name
-        
+
         self.weapon_anim = items[weapon_name].weapon_anim
         self.weapon_sprite = items[weapon_name].weapon_sprite
         self.add(self.weapon_sprite)
-        
+
     def on_mouse_press(self, x, y, button, modifiers):
         if button & mouse.LEFT:
-            self.weapon_sprite.image = Animation.from_image_sequence(self.shoot_grid[:], 0.05, loop=False)    
-    
+            self.weapon_sprite.image = self.weapon_anim
+
     def recharge(self, bulletType):
         # bulletType - патроны определенного типа в инвентаре
         # .count - кол-во патронов данного типа в нвентаре
         bulletType.count -= self.max_cartridge - self.cartridge
         self.cartridge = self.max_cartridge
+
