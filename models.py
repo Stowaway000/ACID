db5fd3038fd9340221bb6fb37bfebb712ecb6965
Konnov Konstantin <boneyandoom@gmail.com>
2019-04-21 18:35:15 +0300

    added inventory class

--- models.py
+++ models.py
@@ -1,13 +1,50 @@
 import cocos
 from cocos.director import director
 
+def get_weight(item):
+    if item in items:
+        return items[item].weight
+    elif item in weapons:
+        return weapons[item].weight
+    elif item in armors:
+        return armors[item].weight
+
+
+class inventory():
+    def __init__(self):
+        self.weight = 0
+        self.items = []
+        self.weapons = []
+        self.armors = []
+
+    def add(self, item):
+        if item in items:
+            if item in self.items:
+                self.items[item] += 1
+            else:
+                self.items[item] = 1
+        elif item in weapons:
+            self.weapons.append(weapon_handler(item))
+        elif item in armors:
+            self.armors.append(armor_handler(item))
+
+    def get(self, item):
+        return self.items[i]
+
+    def get_armor(i):
+        return self.armors[i]
+
+    def get_weapon(i):
+        return self.weapons[i]
+
+
 class character(cocos.layer.ScrollableLayer):
-    def __init__(self, name, fraction):
+    def __init__(self, name, fraction, seacil):
         self.photo = cocos.sprite.Sprite('res/img/portraits/' + name + '.png')
 
-        self.inventory = []
-        self.armors = []
-        self.weapons = []
+        self.SEACIL = seacil
+        
+        self.inventory = inventory()
         
         self.weapon_left = -1
         self.weapon_right = -1
@@ -20,17 +57,17 @@ class character(cocos.layer.ScrollableLayer):
 
         self.fraction = fraction
 
-    def take_damage(dmg, k):
+    def take_damage(self, dmg, k):
         if self.armor != -1:
-            dmg -= self.armors[armor].statusAC(dmg, k)
+            dmg -= self.inventory.get_armor(armor).statusAC(dmg, k)
 
         if dmg > 0:
             self.hp -= dmg
 
-    def use_ability():
+    def use_ability(self):
         pass
 
-    def switch_weapon():
+    def switch_weapon(self):
         if self.weapon_left == -1 and self.weapon_right == -1:
             self.weapon_left == self.weapon_l_equip
             self.weapon_right == self.weapon_r_equip
@@ -38,36 +75,31 @@ class character(cocos.layer.ScrollableLayer):
             self.weapon_left == -1
             self.weapon_right == -1
 
-    def attack(hand):
+    def attack(self, hand):
         if hand == 'r' and self.weapon_right != -1:
-            self.weapons[self.weapon_right].shoot()
+            self.inventory.get_weapon(self.weapon_right).shoot()
         elif self.weapon_left != -1:
-            self.weapons[self.weapon_left].shoot()
+            self.inventory.get_weapon(self.weapon_left).shoot()
             
-    def reload(hand):
+    def reload(self, hand):
         if hand == 'r' and self.weapon_right != -1:
-            ammo, ammo_type = self.weapon.get_max_cartridge()
+            ammo, ammo_type = self.inventory.get_weapon(self.weapon_right)\
+                              .get_max_cartridge()
             ammo = self.get_bullets(ammo, ammo_type)
-            self.weapons[self.weapon_right].recharge(ammo)
+            self.inventory.get_weapon(self.weapon_right).recharge(ammo)
         elif self.weapon_left != -1:
-            ammo, ammo_type = self.weapon.get_max_cartridge()
+            ammo, ammo_type = self.inventory.get_weapon(self.weapon_left)\
+                              .get_max_cartridge()
             ammo = self.get_bullets(ammo, ammo_type)
-            self.weapons[self.weapon_left].recharge(ammo)
+            self.inventory.get_weapon(self.weapon_left).recharge(ammo)
 
-    def take_item(item, count):
-        #if get_weight(item) + self 
-        if item in items:
-            if item in self.inventory:
-                self.inventory[item] += 1
-            else:
-                self.inventory[item] = 1
-        elif item in armors:
-            self.weapons[item] = armor_handler(item)
-        elif item in weapons:
-            self.weapons[item] = weapon_handler(item)
+    def take_item(self, item, count):
+        for i in range(count):
+            if self.inventory.weight + get_weight(item) < 4*self.SEACIL[0]:
+                self.inventory.add(item)
 
-    def drop_item():
+    def drop_item(self):
         pass
 
-    def store_item():
+    def store_item(self):
         pass
