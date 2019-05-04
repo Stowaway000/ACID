f500254d2c6912a713e64800639d845d8d99f9f3
Konnov Konstantin <boneyandoom@gmail.com>
2019-05-04 15:17:28 +0300

    #10 bars with pics

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import cocos
 from cocos.sprite import Sprite
+from cocos.text import Label
 
 
 class interface(cocos.layer.Layer):
@@ -9,7 +10,12 @@ class interface(cocos.layer.Layer):
         super().__init__()
 
         for key, val in stats.items():
-            self.bars[key] = cocos.text.Label(str(val[0]), val[1])
+            self.bars[key] = Label(str(val[0]), val[1], font_name='Calibri',\
+                                   color=(229, 23, 20, 255), anchor_x='center'\
+                                   , anchor_y='center')
+            spr = Sprite('res/img/interface/' + key + '.png')
+            spr.position = val[1]
+            self.add(spr)
 
         for key, val in self.bars.items():
             self.add(val, name=key)
@@ -22,6 +28,6 @@ class interface(cocos.layer.Layer):
         self.remove(stat)
 
         old_pos = self.bars[stat].position
-        self.bars[stat] = cocos.text.Label(str(new), old_pos)
+        self.bars[stat] = Label(str(new), old_pos)
 
         self.add(self.bars[stat], name=stat)
