40093ba33f327cda170665e5f50e55c96029a73e
Georg <stowaway000@yandex.ru>
2019-05-28 02:08:33 +0300

    #13 Исправлено движение NPC

--- npc.py
+++ npc.py
@@ -27,11 +27,15 @@ class NPC(character):
         self.hp = stats[0]
         self.stamina = stats[1]
         self.sp_stamina = stats[2]
-        for i in range(2):
-            for j in range(2):
-                self.rad_patrol[i][j] = stats[3 + i + j]
+        self.rad_patrol = [[0] * 2 for i in range(2)]
+        self.rad_patrol[0][0] = stats[3]
+        self.rad_patrol[0][1] = stats[4]
+        self.rad_patrol[1][0] = stats[5]
+        self.rad_patrol[1][1] = stats[6]
+        self.next_x = stats[-2]
+        self.next_y = stats[-1]
 
-        super().__init__(name, fraction, stats[3:-1], npc_mover(), stats[-1])
+        super().__init__(name, fraction, stats[7:-2], npc_mover(), (stats[-1], stats[-2]))
 
         self.angle_velocity = 0
         self.state = "patrol"
@@ -59,13 +63,13 @@ class NPC(character):
         pass
 
 
-class patroling():
+class patroling:
     def __init__(self, rad_patrol):
         self.rad_patrol = rad_patrol
 
     def choose_point(self):
         x = randint(self.rad_patrol[0][0], self.rad_patrol[1][0])
-        y = randint(self.rad_patrol[0][1], self.rad_patrol[1][1])
+        y = randint(self.rad_patrol[1][1], self.rad_patrol[0][1])
         return x, y
 
     def trade(self):
@@ -93,23 +97,18 @@ class fight():
 
 
 class npc_mover(cocos.actions.Move):
-    def __init__(self):
-        super().__init__()
-        self.x = self.target.position[0]
-        self.y = self.target.position[1]
-
     def step(self, dt):
-        if self.target.position[0] != self.x and self.target.position[1] != self.y:
-            vel_x = (self.x / hypot(self.x, self.y)) * 50
-            vel_y = (self.y / hypot(self.x, self.y)) * 50
+        if self.target.position[0] != self.target.parent.next_x and self.target.position[1] != self.target.parent.next_y:
+            vel_x = (self.target.parent.x / hypot(self.target.parent.x, self.target.parent.y)) * 50
+            vel_y = (self.target.parent.y / hypot(self.target.parent.x, self.target.parent.y)) * 50
         else:
             vel_x = 0
             vel_y = 0
 
         if self.target.velocity[0] or self.target.velocity[1]:
-            self.target.walk(True)
+            self.target.walker(True)
         elif not (self.target.velocity[0] or self.target.velocity[1]):
-            self.target.walk(False)
+            self.target.walker(False)
 
         dx = vel_x * dt
         dy = vel_y * dt
@@ -124,7 +123,7 @@ class npc_mover(cocos.actions.Move):
         if self.target.collider.collision_manager.any_near(new, 0):
             vel_y = 0
             new.cshape.center.y -= dy
-
+        print(new.cshape.center)
         self.target.parent.info = self.target.parent.get_in_sector(self.target.position,
                                                                    self.target.rotation,
                                                                    20,
@@ -132,9 +131,7 @@ class npc_mover(cocos.actions.Move):
                                                                     sin(self.target.rotation)])
         self.target.velocity = (vel_x, vel_y)
         self.target.position = new.cshape.center
-        self.target.scroller.set_focus(*new.cshape.center)
-
-        self.target.scroller.set_focus(self.target.x, self.target.y)
 
         if vel_x == 0 and vel_y == 0:
-            self.x, self.y = self.target.parent.patrol.choose_point()
+            self.target.parent.next_x, self.target.parent.next_y = \
+                self.target.parent.patrol.choose_point()
